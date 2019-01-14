e1056837e86dbdad63e2b8539d53b59c3b7982dc
beyondszine <saurabhshandilya.1991@gmail.com>
2019-01-15 02:08:41 +0530

    added node notifier object for sending desktop alerts

---
+++ README.md
@@ -0,0 +1,5 @@
+
+### throw your files for watch, changes are pushed to pipieline for operations like filtering,conditioning,operations,alerts etc
+```sh
+tail -f /var/log/auth.log | node stream.js
+```
--- index.js
+++ index.js
@@ -4,6 +4,7 @@
     const fs = require('fs');
     const readline = require('readline');    
     const notifier = require('node-notifier');
+    const path = require('path');
 
     const rl = readline.createInterface({
         input: process.stdin,
@@ -15,7 +16,30 @@
         console.log(`Line from file: ${line}`);
         var attackAttempt=line.match("Failed password");
         console.log("attack attempt detected",attackAttempt);
-        notifier.notify('Attack detected!');
+        notifier.notify(
+            {
+              title: 'Attack Detected!',
+              message: 'Unsuccessfull login attempt, Mr. User!',
+              icon: path.join(__dirname, 'thuglife.jpg'), // Absolute path (doesn't work on balloons)
+              sound: true, // Only Notification Center or Windows Toasters
+              wait: false // Wait with callback, until user action is taken against notification
+            },
+            function(err, response) {
+              // Response is response from notification
+            }
+          );
+           
+          notifier.on('click', function(notifierObject, options) {
+            // Triggers if `wait: true` and user clicks notification
+            console.log('clicked!');  
+          });
+           
+          notifier.on('timeout', function(notifierObject, options) {
+            // Triggers if `wait: true` and notification closes
+            console.log('time out!');
+        });
+
+        // notifier.notify('Attack detected!');
 
     })
     .on('close', () => {
